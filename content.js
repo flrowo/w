804cd313b189ab77551fc0e6e64f27a7b4743e61
unknown <joaopedrolibonati@gmail.com>
2020-09-02 00:33:57 +0000

    post types, small layout update, back button, more

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 class Content {
-    constructor(title, text, date){
+    constructor(title, text, date, type){
         this.title = title;
         this.text = text;
         this.date = date;
+        this.type = type;
     }
 } 
 
@@ -11,16 +12,27 @@ let content = [];
 let allright = false;
 
 
-$.ajax({url: "./zsources/blog%20text/texto.txt", success: function(result){
-    text = result;
-    allright = true;
-    createContent(true);
-}, error: function(){createContent(false); console.log("couldn't get the online .txt")}
-});
 
-function createContent(hasText){
+/* ajax vanilla
+
+fetch(new Request('https://parallelum.com.br/fipe/api/v1/carros/marcas', {method: 'GET'}))
+.then(response => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error('Ops! Houve um erro em nosso servidor.');
+    }
+})
+.then(response => { console.log(response); })
+.catch(error => { console.error(error); });
+*/
+
+function createContent(hasText, type){
+    console.log("type: "+type);
     if(hasText == false){
-        text = `T> I made an osu!Taiko Hack - hackdetaco
+        text = `
+
+        TITLE> I made an osu!Taiko Hack - hackdetaco
 p> There is more info at <a href="https://github.com/calmylake/hackdetaco">github.com/calmylake/hackdetaco</a>.
 I did it just today, I feel proud of finally doing something from start to finish. 
 But also... I just wanna go sleep...
@@ -28,7 +40,9 @@ But also... I just wanna go sleep...
 <img src="https://media.tenor.com/images/61dcf7e4d65440b06c8c901ace9924a6/tenor.gif">
 DATE> 31/08/2020
 
-T> UM TÍTULO MUUU U UU U UU UUUUUU ITO GRANDE
+TYPE> tools
+
+TITLE> UM TÍTULO MUUU U UU U UU UUUUUU ITO GRANDE
 p> aaaaa 
 hoje eu fiz um site
 construi do zero muleke
@@ -39,8 +53,10 @@ blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahb
 aaaaaaaaaa
 DATE> 28/08/2020
 
+TYPE> jooj
 
-T> UM TÍTULO
+
+TITLE> UM TÍTULO
 p> blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah 
 blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah 
 blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah 
@@ -54,16 +70,17 @@ blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahb
 blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah 
 blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah 
 
-
+TYPE> jooj
 DATE> 27/08/2020
+
 `;
     }
 
 
-    content = text.split("T>");
-    //filtered is the content with only the items that have "T>", "p>" and "DATE>"
+    content = text.split("TITLE>");
+    //filtered is the content with only the items that have "TITLE>", "p>" and "DATE>"
     let filtered = content.filter(function(el){
-        return (el != "" && el != null && (el.includes("p>") && el.includes("DATE>")));
+        return (el != "" && el != null && (el.includes("p>") && el.includes("DATE>") && el.includes("TYPE>")));
     });
     
     let actContent = [];
@@ -71,28 +88,50 @@ DATE> 27/08/2020
     (() => {
         let i = 0;
         while(i < filtered.length){
-            let title = filtered[i].split("p>")[0].trim();
-            let text = filtered[i].split("p>")[1].split("DATE>")[0].trim().replace(/\n/g,"<br>");
-            let date = "<br><br>"+filtered[i].split("p>")[1].split("DATE>")[1].trim();
-            actContent.push(new Content(title, text, date));
+            let title = filtered[i].split("\n")[0].trim();
+            let text = filtered[i].split("p>")[1].split(/DATE>|TYPE>/)[0].trim().replace(/\n/g,"<br>");
+            let date = filtered[i].split("DATE>")[1].split("\n")[0].trim();
+            let type = filtered[i].split("TYPE>")[1].split("\n")[0].toLowerCase().trim();
+            actContent.push(new Content(title, text, date, type));
             i++;
         }
     })();
     
     (() => {
         let i = 0;
-        let htmlstring = ""
+        let htmlstring = "";
+        let allContent = false;
+        if(type == "all"){
+            allContent = true;
+        } else{
+            htmlstring += `<button onclick="updateContent();">back</button>`;
+        }
         while(i < actContent.length){
-            htmlstring += `<div class="content-item">
-            <h1>${actContent[i].title}</h1>
-            <hr>
-            ${actContent[i].text}
-            <br>
-            ${actContent[i].date}
-            <br><br></div><br>`;
+            
+            if(allContent || actContent[i].type == type){
+                htmlstring += `<div class="content-item">
+                <h1>${actContent[i].title}</h1><br>
+                ${actContent[i].type} - ${actContent[i].date}
+                <hr>
+                ${actContent[i].text}
+                <br><br>
+                </div>`;
+            }
             i++;
         }
         document.getElementById("content").innerHTML = htmlstring.trim();
     })();
 
 }
+
+function updateContent(contentType = "all"){
+    $.ajax({url: "./zsources/blog%20text/root.txt", success: function(result){
+        text = result;
+        allright = true;
+        console.log("contentType: "+contentType);
+        createContent(true, contentType);
+    }, error: function(){createContent(false, contentType); console.log("couldn't get the online .txt")}
+    });
+}
+
+updateContent();
